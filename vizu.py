a82cf64a04670d8ba98e8bfe54ec48f0468b65f0
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-02-10 00:40:42 +0530

    Add files via upload

---
+++ vizu.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import plotly_express as px
+import pandas as pd
+
+
+
+
+# configuration
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+# title of the app
+st.title("VISUALIZER 📈📊📉")
+
+# Add a sidebar
+st.sidebar.subheader("File Upload")
+
+# Setup file upload
+uploaded_file = st.sidebar.file_uploader(
+                        label="Upload your CSV or Excel file. (200MB max)",
+                         type=['csv', 'xlsx'])
+
+global df
+if uploaded_file is not None:
+    print(uploaded_file)
+    
+
+    try:
+        df = pd.read_csv(uploaded_file)
+    except Exception as e:
+        print(e)
+        df = pd.read_excel(uploaded_file)
+
+global numeric_columns
+global non_numeric_columns
+try:
+    st.write(df)
+    numeric_columns = list(df.select_dtypes(['float', 'int']).columns)
+    non_numeric_columns = list(df.select_dtypes(['object']).columns)
+    non_numeric_columns.append(None)
+    print(non_numeric_columns)
+except Exception as e:
+    print(e)
+    st.write("Please upload your file to the application.")
+
+# add a select widget to the side bar
+chart_select = st.sidebar.selectbox(
+    label="Select the chart type",
+    options=['Scatterplots', 'Lineplots', 'Histogram', 'Boxplot','Piechart']
+)
+
+if chart_select == 'Scatterplots':
+    st.sidebar.subheader("Scatterplot Settings")
+    try:
+        x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
+        y_values = st.sidebar.selectbox('Y axis', options=numeric_columns)
+        color_value = st.sidebar.selectbox("Color", options=non_numeric_columns)
+        plot = px.scatter(data_frame=df, x=x_values, y=y_values, color=color_value)
+        # display the chart
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
+
+if chart_select == 'Lineplots':
+    st.sidebar.subheader("Line Plot Settings")
+    try:
+        x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
+        y_values = st.sidebar.selectbox('Y axis', options=numeric_columns)
+        color_value = st.sidebar.selectbox("Color", options=non_numeric_columns)
+        plot = px.line(data_frame=df, x=x_values, y=y_values, color=color_value)
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
+
+if chart_select == 'Histogram':
+    st.sidebar.subheader("Histogram Settings")
+    try:
+        x = st.sidebar.selectbox('Feature', options=numeric_columns)
+        bin_size = st.sidebar.slider("Number of Bins", min_value=10,
+                                     max_value=100, value=40)
+        color_value = st.sidebar.selectbox("Color", options=non_numeric_columns)
+        plot = px.histogram(x=x, data_frame=df, color=color_value)
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
+
+if chart_select == 'Boxplot':
+    st.sidebar.subheader("Boxplot Settings")
+    try:
+        y = st.sidebar.selectbox("Y axis", options=numeric_columns)
+        x = st.sidebar.selectbox("X axis", options=non_numeric_columns)
+        color_value = st.sidebar.selectbox("Color", options=non_numeric_columns)
+        plot = px.box(data_frame=df, y=y, x=x, color=color_value)
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
+
+if chart_select == 'Piechart':
+    st.sidebar.subheader("Piechart Settings")
+    try:
+        x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
+        y_values = st.sidebar.selectbox('Y axis', options=non_numeric_columns)
+        
+        plot = px.pie(data_frame=df, values=x_values, names=y_values)
+        # display the chart
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
+        
+st.sidebar.write('Made by MAINAK CHAUDHURI')
